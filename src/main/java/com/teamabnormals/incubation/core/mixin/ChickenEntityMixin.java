d2280dbfa092cfcc69fcc458df6fcb078bcfd857
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-01-02 12:01:04 -0600

    i forgor chicken egg mixin

---
+++ src/main/java/com/teamabnormals/incubation/core/mixin/ChickenEntityMixin.java
@@ -0,0 +1,59 @@
+package com.teamabnormals.incubation.core.mixin;
+
+import com.teamabnormals.incubation.api.EggLayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Random;
+
+@Mixin(Chicken.class)
+public abstract class ChickenEntityMixin extends Animal implements EggLayer {
+
+	protected ChickenEntityMixin(EntityType<? extends Animal> type, Level worldIn) {
+		super(type, worldIn);
+	}
+
+	@Shadow
+	public int eggTime;
+
+	@Shadow
+	public boolean isChickenJockey;
+
+	@Override
+	public int getEggTimer() {
+		return this.eggTime;
+	}
+
+	@Override
+	public void setEggTimer(int time) {
+		this.eggTime = time;
+	}
+
+	@Override
+	public boolean isBirdJockey() {
+		return this.isChickenJockey;
+	}
+
+	@Override
+	public Item getEggItem() {
+		return Items.EGG;
+	}
+
+	@Override
+	public int getNextEggTime(Random rand) {
+		return rand.nextInt(6000) + 6000;
+	}
+
+	@Override
+	public SoundEvent getEggLayingSound() {
+		return SoundEvents.CHICKEN_EGG;
+	}
+}

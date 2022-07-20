afd94db19afdbda287d5c1adf1d534a49fd2aa11
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2022-07-20 17:40:51 +0300

    Scrambled egg heart particles
    Eating scrambled eggs now produces heart particles to show that they heal you.

--- src/main/java/com/teamabnormals/incubation/common/item/ScrambledEggsItem.java
+++ src/main/java/com/teamabnormals/incubation/common/item/ScrambledEggsItem.java
@@ -1,5 +1,8 @@
 package com.teamabnormals.incubation.common.item;
 
+import java.util.Random;
+
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.BowlFoodItem;
 import net.minecraft.world.item.Item;
@@ -14,6 +17,15 @@ public class ScrambledEggsItem extends BowlFoodItem {
 	@Override
 	public ItemStack finishUsingItem(ItemStack stack, Level worldIn, LivingEntity entityLiving) {
 		entityLiving.heal(2.0F);
+		Random rand = entityLiving.getRandom();
+		if (worldIn.isClientSide()) {
+			for (int i = 0; i < 4; ++i) {
+				double d0 = rand.nextGaussian() * 0.02D;
+				double d1 = rand.nextGaussian() * 0.02D;
+				double d2 = rand.nextGaussian() * 0.02D;
+				worldIn.addParticle(ParticleTypes.HEART, entityLiving.getRandomX(1.0D), entityLiving.getRandomY() + 0.5D, entityLiving.getRandomZ(1.0D), d0, d1, d2);
+			}
+		}
 		return super.finishUsingItem(stack, worldIn, entityLiving);
 	}
 }

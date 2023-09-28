aebcd78ae95cb527f065aedcfdc7e6389ab4e7db
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-09-27 21:16:20 -0500

    Tweak chicken nest generation

--- src/main/java/com/teamabnormals/incubation/core/data/server/tags/IncubationBiomeTagsProvider.java
+++ src/main/java/com/teamabnormals/incubation/core/data/server/tags/IncubationBiomeTagsProvider.java
@@ -6,6 +6,7 @@ import com.teamabnormals.incubation.core.other.tags.IncubationBiomeTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BiomeTagsProvider;
 import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biomes;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class IncubationBiomeTagsProvider extends BiomeTagsProvider {
@@ -16,7 +17,7 @@ public class IncubationBiomeTagsProvider extends BiomeTagsProvider {
 
 	@Override
 	protected void addTags() {
-		this.tag(IncubationBiomeTags.HAS_CHICKEN_NEST).addTag(BiomeTags.IS_FOREST);
+		this.tag(IncubationBiomeTags.HAS_CHICKEN_NEST).add(Biomes.FOREST, Biomes.FLOWER_FOREST, Biomes.BIRCH_FOREST, Biomes.OLD_GROWTH_BIRCH_FOREST, Biomes.DARK_FOREST).addTag(BiomeTags.IS_JUNGLE).addTag(BiomeTags.IS_TAIGA);
 		this.tag(IncubationBiomeTags.HAS_DUCK_NEST).addOptionalTag(IncubationConstants.HAS_DUCK);
 		this.tag(IncubationBiomeTags.HAS_TURKEY_NEST).addOptionalTag(IncubationConstants.HAS_TURKEY);
 	}
